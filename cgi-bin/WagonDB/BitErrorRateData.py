8b632945845e54e089aa6fa17f1ba31236064393
Bryan Crossman <cros0400@umn.edu>
2023-05-30 15:20:10 -0500

    Cleaned up interface for plot pages

--- cgi-bin/WagonDB/BitErrorRateData.py
+++ cgi-bin/WagonDB/BitErrorRateData.py
@@ -5,6 +5,7 @@ import cgitb
 import base
 import module_functions
 import sys
+import plotheader
 #import makePlots as mp
 #makePlot arugments:(Test, Data, Board, SN, BitError, Tester)
 
@@ -19,121 +20,113 @@ else:
 
 base.header(title='Bit Error Rate Data')
 base.top()
+plotheader.plotscript()
 
 print('<div class="row">')
-print('''
-<div>       
-    <button class="btn btn-secondary dropdown-toggle" type="button" id="SelectTest" data-bs-toggle="dropdown" aria-expanded="false">
-        Test
-    </button>
-    <ul class="dropdown-menu" aria-labelledby="SelectTest">
-        <li><a class="dropdown-item" href="#">Resistance Measurement</a></li>
-        <li><a class="dropdown-item" href="#">ID Resistor</a></li>
-        <li><a class="dropdown-item" href="#">I2C Read/Write</a></li>
-        <li><a class="dropdown-item" href="#">Bit Error Rate</a></li>
-    </ul>
-
-    <button class="multiselect dropdown-toggle btn btn-secondary" type="button" id="SelectBoards" data-bs-toggle="dropdown" aria-expanded="false">
-        <span class="multiselect-selected-text">
-            Select Board
-        </span>
-    </button>
-    <ul class="dropdown-menu" aria-labelledby="SelectBoards">
-        <li><a class="dropdown-item" href="#">WW20A1</a></li>
-        <li><a class="dropdown-item" href="#">WW10A1</a></li>
-        <li><a class="dropdown-item" href="#">WE10A1</a></li>
-        <li><a class="dropdown-item" href="#">WE20B1</a></li>
-    </ul>
-    <button class="btn btn-secondary dropdown-toggle" type="button" id="SelectTest" data-bs-toggle="dropdown" aria-expanded="false">
-        Tester
-    </button>
-    <ul class="dropdown-menu" aria-labelledby="SelectTester">
-        <li><a class="dropdown-item" href="#">Rand</a></li>
-        <li><a class="dropdown-item" href="#">Bryan</a></li>
-    </ul>
-</div>
-''')
+plotheader.plotheader()
 print('''
 <div>
-    <img src="../../static/files/Bit_Error_Rate_Midpoint.png">
-</div>
-<div>
-    <img src="../../static/files/Bit_Error_Rate_EyeOpening.png"> 
-</div>
-<div>
-<div style="float:left">
 <table>
-    <tr><td>
-        <input class="form-check-input" type="checkbox" value="">
-        <label class="form-check-label">
-            Clock 0
-        </label>
-    </td></tr>
-    <tr><td>
-        <input class="form-check-input" type="checkbox" value="">
-        <label class="form-check-label">
-            Clock 1
-        </label>
-    </td></tr>
-    <tr><td>
-        <input class="form-check-input" type="checkbox" value="">
-        <label class="form-check-label">
-            Clock 2
-        </label>
-    </td></tr>
-    <tr><td>
-        <input class="form-check-input" type="checkbox" value="">
-        <label class="form-check-label">
-            Trigger 0
-        </label>
-    </td></tr>
-    <tr><td>
-        <input class="form-check-input" type="checkbox" value="">
-        <label class="form-check-label">
-            Trigger 1
-        </label>
-    </td></tr>
-    <tr><td>
-        <input class="form-check-input" type="checkbox" value="">
-        <label class="form-check-label">
-            Trigger 2
-        </label>
-    </td></tr>
-    <tr><td>
-        <input class="form-check-input" type="checkbox" value="">
-        <label class="form-check-label">
-            Trigger 3
-        </label>
-    </td></tr>
-    <tr><td>
-        <input class="form-check-input" type="checkbox" value="">
-        <label class="form-check-label">
-            Trigger 4
-        </label>
-    </td></tr>
-    <tr><td>
-        <input class="form-check-input" type="checkbox" value="">
-        <label class="form-check-label">
-            Trigger 5
-        </label>
-    </td></tr>
-    <tr><td>
-        <input class="form-check-input" type="checkbox" value="">
-        <label class="form-check-label">
-            Trigger 6
-        </label>
-    </td></tr>
-    <tr><td>
-        <input class="form-check-input" type="checkbox" value="">
-        <label class="form-check-label">
-            Trigger 7
-        </label>
-    </td></tr>
+<tr>
+<td>
+    <img src="../../static/files/Bit_Error_Rate_Midpoint.png">
+</td>
+<td>
+    <img src="../../static/files/Bit_Error_Rate_EyeOpening.png"> 
+</td>
+<td>
+<div style="padding-left:5px">
+    <table>
+        <th> Select Components </th>
+        <tr><td style="padding-bottom:3px">
+            <input class="form-check-input" type="checkbox" value="">
+            <label class="form-check-label">
+                Clock 0
+            </label>
+        </td></tr>
+        <tr><td style="padding-bottom:3px">
+            <input class="form-check-input" type="checkbox" value="">
+            <label class="form-check-label">
+                Clock 1
+            </label>
+        </td></tr>
+        <tr><td style="padding-bottom:3px">
+            <input class="form-check-input" type="checkbox" value="">
+            <label class="form-check-label">
+                Clock 2
+            </label>
+        </td></tr>
+        <tr><td style="padding-bottom:3px">
+            <input class="form-check-input" type="checkbox" value="">
+            <label class="form-check-label">
+                Trigger 0
+            </label>
+        </td></tr>
+        <tr><td style="padding-bottom:3px">
+            <input class="form-check-input" type="checkbox" value="">
+            <label class="form-check-label">
+                Trigger 1
+            </label>
+        </td></tr>
+        <tr><td style="padding-bottom:3px">
+            <input class="form-check-input" type="checkbox" value="">
+            <label class="form-check-label">
+                Trigger 2
+            </label>
+        </td></tr>
+        <tr><td style="padding-bottom:3px">
+            <input class="form-check-input" type="checkbox" value="">
+            <label class="form-check-label">
+                Trigger 3
+            </label>
+        </td></tr>
+        <tr><td style="padding-bottom:3px">
+            <input class="form-check-input" type="checkbox" value="">
+            <label class="form-check-label">
+                Trigger 4
+            </label>
+        </td></tr>
+        <tr><td style="padding-bottom:3px">
+            <input class="form-check-input" type="checkbox" value="">
+            <label class="form-check-label">
+                Trigger 5
+            </label>
+        </td></tr>
+        <tr><td style="padding-bottom:3px">
+            <input class="form-check-input" type="checkbox" value="">
+            <label class="form-check-label">
+                Trigger 6
+            </label>
+        </td></tr>
+        <tr><td style="padding-bottom:3px">
+            <input class="form-check-input" type="checkbox" value="">
+            <label class="form-check-label">
+                Trigger 7
+            </label>
+        </td></tr>
+    </table>
+</div>
+</td>
+<td>
+<div style="padding-left:5px">
+    <table>
+        <th> Select Outcome </th>
+        <tr><td style="padding-bottom:3px">
+            <input class="form-check-input" type="checkbox" value="" checked=True>
+            <label class="form-check-label">
+                Successful
+            </label>
+        </td></tr>    
+        <tr><td style="padding-bottom:3px">
+            <input class="form-check-input" type="checkbox" value="" checked=True>
+            <label class="form-check-label">
+                Unsuccessful
+            </label>
+        </td></tr>    
+    </table>
+</div> 
+</tr>
 </table>
-</div>
-<div style="float:leftt">
-    <button> Update Plot </button>
-</div>
 </div>
 ''')
 print('</div>')
--- cgi-bin/WagonDB/ResistanceMeasurementData.py
+++ cgi-bin/WagonDB/ResistanceMeasurementData.py
@@ -5,6 +5,7 @@ import cgitb
 import base
 import module_functions
 import sys
+import plotheader
 #import makePlots as mp
 #makePlot arguments:(Test, Data, Board, SN, BitError, Tester)
 
@@ -19,114 +20,86 @@ else:
 
 base.header(title='Resistance Measurement Data')
 base.top()
+plotheader.plotscript()
 
 print('<div class="row">')
-print('''
-<div>       
-    <button class="btn btn-secondary dropdown-toggle" type="button" id="SelectTest" data-bs-toggle="dropdown" aria-expanded="false">
-        Test
-    </button>
-    <ul class="dropdown-menu" aria-labelledby="SelectTest">
-        <li><a class="dropdown-item" href="#">Resistance Measurement</a></li>
-        <li><a class="dropdown-item" href="#">ID Resistor</a></li>
-        <li><a class="dropdown-item" href="#">I2C Read/Write</a></li>
-        <li><a class="dropdown-item" href="#">Bit Error Rate</a></li>
-    </ul>
-
-    <button class="multiselect dropdown-toggle btn btn-secondary" type="button" id="SelectBoards" data-bs-toggle="dropdown" aria-expanded="false">
-        <span class="multiselect-selected-text">
-            Select Board
-        </span>
-    </button>
-    <ul class="dropdown-menu" aria-labelledby="SelectBoards">
-        <li><a class="dropdown-item" href="#">WW20A1</a></li>
-        <li><a class="dropdown-item" href="#">WW10A1</a></li>
-        <li><a class="dropdown-item" href="#">WE10A1</a></li>
-        <li><a class="dropdown-item" href="#">WE20B1</a></li>
-    </ul>
-    <button class="btn btn-secondary dropdown-toggle" type="button" id="SelectTest" data-bs-toggle="dropdown" aria-expanded="false">
-        Tester
-    </button>
-    <ul class="dropdown-menu" aria-labelledby="SelectTester">
-        <li><a class="dropdown-item" href="#">Rand</a></li>
-        <li><a class="dropdown-item" href="#">Bryan</a></li>
-    </ul>
-</div>
-''')
+plotheader.plotheader()
 print('''
 <div>
-<div style="float:left">
-    <img src="../../static/files/Resistance_Measurement.png">
+<div style="float:left; padding-right:10px">
+    <img src="../../static/files/Resistance_Measurement.png?ver=1.2">
 </div>
-<div style="float:leftt">
+<div>
+<div style="float:left; padding-top:5px;">
 <table>
-    <tr><td>
+    <th> Select Pathway </th>
+    <tr><td style="padding-bottom:3px">
         <input class="form-check-input" type="checkbox" value="">
         <label class="form-check-label">
             RTD -> VMON_LVS Module 1
         </label>
     </td></tr>
-    <tr><td>
+    <tr><td style="padding-bottom:3px">
         <input class="form-check-input" type="checkbox" value="">
         <label class="form-check-label">
             RTD -> VMON_LVS Module 2
         </label>
     </td></tr>
-    <tr><td>
+    <tr><td style="padding-bottom:3px">
         <input class="form-check-input" type="checkbox" value="">
         <label class="form-check-label">
             ECON_RE_Sb -> HGCROC_RE_Sb Module 1
         </label>
     </td></tr>
-    <tr><td>
+    <tr><td style="padding-bottom:3px">
         <input class="form-check-input" type="checkbox" value="">
         <label class="form-check-label">
             ECON_RE_Sb -> HGCROC_RE_Sb Module 2
         </label>
     </td></tr>
-    <tr><td>
+    <tr><td style="padding-bottom:3px">
         <input class="form-check-input" type="checkbox" value="">
         <label class="form-check-label">
             PWR_EN -> PG_LDO Module 1
         </label>
     </td></tr>
-    <tr><td>
+    <tr><td style="padding-bottom:3px">
         <input class="form-check-input" type="checkbox" value="">
         <label class="form-check-label">
             PWR_EN -> PG_LDO Module 2
         </label>
     </td></tr>
-    <tr><td>
+    <tr><td style="padding-bottom:3px">
         <input class="form-check-input" type="checkbox" value="">
         <label class="form-check-label">
             RTD -> HGCROC_RE_Sb Module 1
         </label>
     </td></tr>
-    <tr><td>
+    <tr><td style="padding-bottom:3px">
         <input class="form-check-input" type="checkbox" value="">
         <label class="form-check-label">
             RTD -> HGCROC_RE_Sb Module 2
         </label>
     </td></tr>
-    <tr><td>
+    <tr><td style="padding-bottom:3px">
         <input class="form-check-input" type="checkbox" value="">
         <label class="form-check-label">
             HGCROC_RE_Hb -> HGCROC_RE_Sb Module 1
         </label>
     </td></tr>
-    <tr><td>
+    <tr><td style="padding-bottom:3px">
         <input class="form-check-input" type="checkbox" value="">
         <label class="form-check-label">
             HGCROC_RE_Hb -> HGCROC_RE_Sb Module 2
         </label>
     </td></tr>
-    <tr><td>
+    <tr><td style="padding-bottom:3px">
         <input class="form-check-input" type="checkbox" value="">
         <label class="form-check-label">
             PG_DCDC -> HGCROC_RE_Sb Module 1
         </label>
     </td></tr>
-    <tr><td>
+    <tr><td style="padding-bottom:3px">
         <input class="form-check-input" type="checkbox" value="">
         <label class="form-check-label">
             PG_DCDC -> HGCROC_RE_Sb Module 2
@@ -134,8 +107,23 @@ print('''
     </td></tr>
 </table>
 </div>
-<div style="float:leftt">
-    <button> Update Plot </button>
+<div>
+<table>
+    <th style="float:leftt; padding-top:5px"> Select Outcome </th>
+    <tr><td style="padding-bottom:3px">
+        <input class="form-check-input" type="checkbox" value="" checked=True>
+        <label class="form-check-label">
+            Successful
+        </label>
+    </td></tr>
+    <tr><td style="padding-bottom:3px">
+        <input class="form-check-input" type="checkbox" value="" checked=True>
+        <label class="form-check-label">
+            Unsuccessful
+        </label>
+    </td></tr>
+</table>
+</div>
 </div>
 </div>
 
