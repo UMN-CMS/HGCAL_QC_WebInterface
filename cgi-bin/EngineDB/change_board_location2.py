b80f6229e11994ab65c75be0fd555ca9d3c8902c
Bryan Crossman <cros0400@umn.edu>
2024-07-23 14:27:55 -0500

    Fixed a couple bugs with the Engine Webpage

---
+++ cgi-bin/EngineDB/change_board_location2.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+import module_functions
+import cgi
+import base
+
+#cgi header
+print("Content-type: text/html\n")
+
+form = cgi.FieldStorage()
+bc = cgi.escape(form.getvalue("full_id"))
+location = cgi.escape(form.getvalue("location"))
+
+base.header(title='Change Board Location')
+base.top(False)
+try:
+    module_functions.change_board_location(str(bc), location)
+except Exception as e:
+    print("Issue updating board location")
+    print(e)
+
+base.bottom(False)
