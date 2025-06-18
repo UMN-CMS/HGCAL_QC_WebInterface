8c8a1ce0fcfac4ec273b7beba0d095f0aacdce71
Jeremiah Mans <jmmans@umn.edu>
2025-06-18 15:26:28 -0500

    Developing

---
+++ cgi-bin/WagonDB/components.py
@@ -0,0 +1,85 @@
+#!/usr/bin/python3
+
+#
+# This script handles all requests related to components
+#
+
+
+import cgi, html
+import base
+from connect import connect
+import settings
+import os.path
+import sys
+import json
+import cgitb
+
+cgitb.enable()
+
+def add_component(cur,barcode,typecode):
+    try:
+        cur.execute("INSERT INTO COMPONENT_STOCK (barcode, typecode) VALUES (?,?)",(barcode,typecode,))
+        return (200,"Ok",)
+    except mariadb.Error as e:
+        return (400,f"Error on add_component: {e}")    
+
+def get_used_for(cur,barcode):
+    retval=[]
+    try:
+        cur.execute("SELECT barcode,typecode from COMPONENT_STOCK JOIN COMPONENT_USAGE WHERE COMPONENT_STOCK.component_id=COMPONENT_USAGE.component_id AND COMPONENT_USAGE.used_in_barcode LIKE ?",(barcode,))
+        for (barcode,typecode) in cur:
+            retval.append(typecode,barcode)
+        return (200,retval,)
+    except mariadb.Error as e:
+        return (400,f"Error on get_used_for: {e}")
+
+def get_unused_stock(cur,typecode,quantity=1):
+    try:
+        if typecode is None:
+            cur.execute("SELECT barcode FROM COMPONENT_STOCK WHERE component_id NOT IN (SELECT component_id from COMPONENT_USAGE) ORDER BY entered LIMIT ?",(quantity,))
+        else:
+            cur.execute("SELECT barcode FROM COMPONENT_STOCK WHERE typecode=? AND component_id NOT IN (SELECT component_id from COMPONENT_USAGE) ORDER BY entered LIMIT ?",(typecode,quantity,))
+        retval=[]
+        for (barcode) in cur:
+            retval.append(barcode)
+        return (200,retval,)    
+    except mariadb.Error as e:
+        return (400,f"Error on get_unused_stock: {e}")
+
+def argas(args,name):
+    if name in args:
+        return args[name]
+    return None
+    
+form = cgi.FieldStorage()
+args={}
+for (key,value) in form:
+    args[key]=value
+#default behavior
+if 'req' not in args:
+    args['req']="get_unused_stock"
+    args['quantity']=20
+db=connect(0)
+cur=db.cursor()
+
+if req in ["get_unused_stock","get_used_for"]:
+    db=connect(0)
+    cur=db.cursor()
+    if req == "get_unused_stock": result=get_unused_stock(cur,argas(args,"typecode"),int(argas(args,"quantity")))
+    if req == "get_used_for": result=get_used_for(cur,argas(args,"barcode"))
+
+if req in ["add_component"]:
+    db=connect(1)
+    cur=db.cursor()
+    if req == "add_component":
+        result=add_component(cur,argas(args,"barcode"),argas(args,"typecode"))
+
+if result[0]>299:
+    print("Content-type: text/plain")
+    print("Status: %d"%(result))
+    print("\n")
+    print(result[1])
+else:
+    print("Content-type: text/json")
+    print("Status: 200 OK\n")
+    print(json.dumps(result[1]))
