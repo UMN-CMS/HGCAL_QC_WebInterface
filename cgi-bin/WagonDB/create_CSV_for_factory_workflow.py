ab779af7b82001c48ef42d382f0245399174cca1
Rand Bovard <bovar008@umn.edu>
2025-07-14 10:46:57 -0500

    updated csv download and label library

--- cgi-bin/WagonDB/create_CSV_for_factory_workflow.py
+++ cgi-bin/WagonDB/create_CSV_for_factory_workflow.py
@@ -16,74 +16,123 @@ print()
 db = connect(0)
 cur=db.cursor()
 
-columns = "'Subtype', 'Number Received', 'Number In Progress', 'Number Awaiting Registration', 'Number Passed and not Shipped', 'Number Shipped', 'Number Failed'"
+columns = '"Subtype", "Total Checked In", "Awaiting Testing", "QC Passed, Awaiting Registration", "Ready for Shipping", "Shipped", "Failed QC", "Shipped to CERN", "Shipped to Fermilab"'
 print(columns)
 
-cur.execute('select distinct type_id from Board order by type_id')
-subtypes = cur.fetchall()
-for s in subtypes:
-    cur.execute('select full_id from Board where type_id="%s"' % s[0])
-    boards = cur.fetchall()
+cur.execute('''
+    select B.full_id, B.type_id, B.board_id, BT.name as nickname, BT.type_id as bt_type_id 
+    from Board B
+    join Board_type BT on B.type_id=BT.type_sn
+    order by B.type_id
+''')
+all_boards = cur.fetchall()
 
-    cur.execute('select type_id from Board_type where type_sn="%s"' % s[0])
-    type_id = cur.fetchall()[0][0]
-    cur.execute('select test_type_id from Type_test_stitch where type_id=%s' % type_id)
-    temp = cur.fetchall()
-    stitch_types = []
-    for test in temp:
-        stitch_types.append(test[0])
-    
-    t_passed = 0
-    t_failed = 0
-    shipped = 0
-    not_registered = 0
-    for b in boards:
+boards_by_type_sn = {}
+board_info = {}
+for full_id, type_sn, board_id, nickname, bt_type_id in all_boards:
+    boards_by_type_sn.setdefault(type_sn, []).append(full_id)
+    board_info[full_id] = {
+            'board_id': board_id,
+            'type_sn': type_sn,
+            'bt_type_id': bt_type_id,
+            'nickname': nickname,
+    }
+
+cur.execute('''
+    select T.board_id, T.test_type_id, T.successful
+    from Test T
+    join (
+        select board_id, test_type_id, MAX(test_id) as latest_test_id
+        from Test
+        group by board_id, test_type_id
+    ) latest on T.test_id = latest.latest_test_id
+''')
+
+test_results = {}
+for board_id, test_type_id, successful in cur.fetchall():
+    test_results.setdefault(board_id, {})[test_type_id] = successful
+
+cur.execute('''
+    select TTS.type_id, TT.test_type, TT.name
+    from Type_test_stitch TTS
+    join Test_Type TT on TTS.test_type_id = TT.test_type
+''')
+stitch_types_by_subtype = {}
+for type_id, test_type_id, test_name in cur.fetchall():
+    stitch_types_by_subtype.setdefault(type_id, []).append((test_type_id, test_name))
+
+cur.execute('select board_id, comment from Check_Out')
+x = cur.fetchall()
+shipped_board_ids = set(row[0] for row in x)
+locations_by_id = {}
+for board_id, comment in x:
+    locations_by_id.setdefault(board_id, {})['Location'] = comment
+
+subtypes = []
+cur.execute('select type_sn from Board_type where type_sn like "WE%" order by type_sn')
+for x in cur.fetchall():
+    subtypes.append(x[0])
+cur.execute('select type_sn from Board_type where type_sn like "WW%" order by type_sn')
+for x in cur.fetchall():
+    subtypes.append(x[0])
+cur.execute('select type_sn from Board_type where type_sn like "WH%" order by type_sn')
+for x in cur.fetchall():
+    subtypes.append(x[0])
+cur.execute('select type_sn from Board_type where type_sn like "ZP%" order by type_sn')
+for x in cur.fetchall():
+    subtypes.append(x[0])
+for type_sn in subtypes:
+    try:
+        boards = boards_by_type_sn[type_sn]
+    except KeyError:
+        print(f'{type_sn}, 0, 0, 0, 0, 0, 0, 0, 0')
+        continue
+
+    bt_type_id = board_info[boards[0]]['bt_type_id']
+    stitch_types = stitch_types_by_subtype.get(bt_type_id, [])
+
+    fermilab = 0
+    cern = 0
+
+    status_map = {}
+    for full_id in boards:
+        board_id = board_info[full_id]['board_id']
         failed = {}
         outcomes = {}
-        # makes an array of falses the length of the number of tests
-        for t in stitch_types:
-            outcomes[t] = False
-            failed[t] = False
+        for test_type_id, test_name in stitch_types:
+            result = test_results.get(board_id, {}).get(test_type_id)
+            outcomes[test_name] = result == 1
+            failed[test_name] = result == 0
 
-        cur.execute('select board_id from Board where full_id="%s"' % b)
-        board_id = cur.fetchall()[0][0]
-        cur.execute('select test_type_id, successful, day from Test where board_id=%s order by day desc, test_id desc' % board_id)
-        temp = cur.fetchall()
-        ids = []
-        for t in temp:
-            if t[0] not in ids:
-                if t[1] == 1:
-                    outcomes[t[0]] = True
-                else:
-                    failed[t[0]] = True
-            ids.append(t[0])
+        num_tests_passed = sum(outcomes.values())
+        num_tests_req = len(outcomes)
+        num_tests_failed = sum(failed.values())
 
-        num = list(outcomes.values()).count(True)
-        total = len(outcomes.values())
-        failed_num = list(failed.values()).count(True)
+        if board_id in shipped_board_ids:
+            status = 'Shipped'
+            location = locations_by_id.get(board_id, {})['Location']
+            if 'Fermilab' in location or 'FNAL' in location:
+                fermilab += 1
+            if 'CERN' in location:
+                cern += 1
 
-        if num == total:
-            t_passed += 1
+        elif num_tests_failed != 0:
+            status = 'Failed'
+        elif num_tests_passed == num_tests_req:
+            status = 'Passed'
+        elif (num_tests_passed == num_tests_req - 1 and not outcomes.get('Registered', False)):
+            status = 'Not Registered'
         else:
-            if (num == total-1 and outcomes[7] == False):
-                not_registered += 1
-        
-        if failed_num != 0:
-            t_failed += 1
-        
-        cur.execute('select board_id from Check_Out where board_id=%s' % board_id)
-        checked_out = cur.fetchall()
-        if checked_out:
-            shipped += 1
-            if num != total:
-                if (num == total-1 and outcomes[7] == False):
-                    not_registered -= 1
-                if failed_num != 0:
-                    t_failed -= 1
-            else:
-                t_passed -= 1
-        
+            status = 'Awaiting'
 
-    awaiting = len(boards) - t_passed - t_failed - shipped - not_registered
-    print("%s, %s, %s, %s, %s, %s, %s" % (s[0], len(boards), awaiting, not_registered, t_passed, shipped, t_failed))
+        status_map[full_id] = status
+
+
+    awaiting = len([k for k,v in status_map.items() if v == 'Awaiting'])
+    not_registered = len([k for k,v in status_map.items() if v == 'Not Registered'])
+    passed = len([k for k,v in status_map.items() if v == 'Passed'])
+    shipped = len([k for k,v in status_map.items() if v == 'Shipped'])
+    failed = len([k for k,v in status_map.items() if v == 'Failed'])
+        
+    print(f'{type_sn}, {len(boards)}, {awaiting}, {not_registered}, {passed}, {shipped}, {failed}, {cern}, {fermilab}')
 
